e22f0166cd2cef095a923461e97dda2c4efeec57
Alexander Behrmann <behrmannalexander@gmail.com>
2013-04-07 15:45:18 +0200

    finish

--- plot_hist.m
+++ plot_hist.m
@@ -1,11 +1,17 @@
+close all
 figure
 hold on
-axis([0 30 0 3500])
+axis([1 30 0 3500])
 for i = 1:78
 plot(HIST_RGB(i,:,3),'b')
 
+end
+
+plot([20 20], [0 3500],'-r')
 title('Histogramm Blau alle Füße','FontSize',16)
 xlabel('Farbwert','FontSize',12)
 ylabel('Häufigkeit','FontSize',12)
 
-end
+
+
+
